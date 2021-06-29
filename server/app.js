c4426a183906181351a1a345f31fb9a5c7460ff8
Tom Virgo <tomcvirgo@gmail.com>
2021-06-29 14:39:18 +0100

    basic server running

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
-//express app here
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+
+//ROUTES
+app.get('/search', (req,res) => {
+    res.json('Hi')
+})
+
+module.exports = { app };
